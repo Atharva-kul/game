28ca60dd1fb2dc3157ae9d1d6b61f654fd74a1ce
Atharva-kul <atharvakulkarni2112005@gmail.com>
2026-02-20 04:58:21 +0530

    adding left and right soldier spawn

--- script.js
+++ script.js
@@ -1,31 +1,63 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
+const rect = canvas.getBoundingClientRect();
+
+canvas.width = rect.width;
+canvas.height = rect.height;
+
+const soldiers = [];
+
 function spawnSoldiers() {
     const blueButton = document.getElementById("spawn_blue");
     const redButton = document.getElementById("spawn_red");
 
+
     blueButton.addEventListener("click", () => {
         // Logic to spawn blue soldiers
-        ctx.fillStyle = "blue";
-        randomSoldierX = Math.random() * canvas.width;
-        randomSoldierY = Math.random() * canvas.height;
-        ctx.fillRect(randomSoldierX, randomSoldierY, 3, 3); // Example drawing of a blue soldier
-        ctx.fillRect(2, 2, 3, 3); // Example drawing of a blue soldier
-
+        let randomSoldierX = Math.random() * (canvas.width)/4;
+        let randomSoldierY = Math.random() * canvas.height;
+        soldiers.push({
+            x: randomSoldierX,
+            y: randomSoldierY,
+            color: "blue",
+            speed: 2
+        });
+        // ctx.fillRect(randomSoldierX, randomSoldierY, 5, 5);
+        }); // Example drawing of a blue soldier
 
         console.log("Spawning blue soldiers...");
-    });
+    
 
     redButton.addEventListener("click", () => {
         // Logic to spawn red soldiers
         ctx.fillStyle = "red";
-        randomSoldierX = Math.random() * canvas.width;
+        // This spawns them in the last 10% of the right side
+        let randomSoldierX = canvas.width - (Math.random() * (canvas.width / 10)) - 10;
         randomSoldierY = Math.random() * canvas.height;
-        ctx.fillRect(randomSoldierX, randomSoldierY, 3, 3); // Example drawing of a red soldier
-        ctx.fillRect(2, 2, 3, 3);
+        soldiers.push({
+            x: randomSoldierX,
+            y: randomSoldierY,
+            color: "red",
+            speed: -2
+        });
+        // ctx.fillRect(randomSoldierX, randomSoldierY, 5, 5); // Example drawing of a red soldier
         console.log("Spawning red soldiers...");
     });
 }
 
+function animate() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    soldiers.forEach(soldier => {
+        // Move soldiers randomly
+        soldier.x += (Math.random() - 0.1) * soldier.speed;
+        //soldier.y += (Math.random() - 0.1) * soldier.speed;
+        ctx.fillStyle = soldier.color;
+        ctx.fillRect(soldier.x, soldier.y, 5, 5);
+    })
+    requestAnimationFrame(animate);
+}
+
+animate();
 spawnSoldiers();
